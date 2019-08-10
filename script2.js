6bdf745e75688c1f14e181d89106e19aee991701
Rafal Pijet <stronglopez@wp.pl>
2019-08-10 16:05:59 +0200

    Add next functions

--- script2.js
+++ script2.js
@@ -1,7 +1,12 @@
 'use strict';
 (function () {
-    let button = document.getElementById("show-button");
+    let button = document.getElementById("count-button");
     let output = document.getElementById("show-output");
+    let removeBox13 = document.getElementById("1");
+    let changeDescription = document.getElementById("2");
+    let changeDescriptionAll = document.getElementById("3");
+    let changeStyleSelected = document.getElementById("4");
+    let removeClass = document.getElementById("5");
 
     const checkNumber = function (number, divisibilityNumber) {
         return number % divisibilityNumber === 0;
@@ -16,5 +21,44 @@
         } else {
             output.innerText = "Liczba " + userNumber + " NIE jest podzielna przez " + divisibilityNumber;
         }
+    });
+
+    removeBox13.addEventListener("click", function () {
+        document.getElementById("box13").remove()
+    });
+
+    changeDescription.addEventListener("click", function () {
+       let boxesWithBg = document.getElementsByClassName("box-bg");
+       let description = window.prompt("Podaj opis:");
+       for (let i = 0; i < boxesWithBg.length; i++) {
+           boxesWithBg[i].innerHTML = description
+       }
+    });
+
+    changeDescriptionAll.addEventListener("click", function () {
+        let headers = document.getElementsByTagName('header');
+        let description = window.prompt("Podaj opis:");
+        let counter = 0;
+
+        while (counter < headers.length) {
+            headers[counter].innerHTML = description;
+            counter ++;
+        }
+    });
+
+    changeStyleSelected.addEventListener("click", function () {
+       let selectedElements = [];
+       let box5Table = document.getElementById("box5").getElementsByTagName("header");
+       selectedElements.push(box5Table[0]);
+       selectedElements.push(document.querySelector("#box7 header"));
+
+       for (let i = 0; i < selectedElements.length; i++) {
+           selectedElements[i].classList.add("header-special");
+       }
+    });
+
+    removeClass.addEventListener("click", function () {
+        document.getElementById("box3").classList.remove("box-bg");
+        document.querySelector("#box5 header").innerHTML = "My header is fancy";
     })
 })();
